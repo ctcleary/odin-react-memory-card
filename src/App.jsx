ddd4dd0ac4d66b89b8e0612816895020b6237fc8
Connor Thomas Cleary <connorthomascleary@gmail.com>
2025-02-15 06:23:59 -0500

    Spruce up the game with a game lost cover flash, etc.

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,8 @@ function App() {
     <h1 id="top-level-headline">The Odin Project - React Memory Card Game Assignment</h1>
     <div id="content-wrapper">
       <div id="content-container">  
-        <p className="rules"><strong>The Rules:</strong> Click each image only once. The game will end if you select the same image twice.</p>
+        <p className="rules"><strong>The Rules:</strong> Click each image only once.<br/>
+        The game will end if you select the same image twice!</p>
         <GameStatus 
           currScore={currScore}
           bestScore={bestScore}
--- src/Game.jsx
+++ src/Game.jsx
@@ -4,47 +4,75 @@ import GIPHY_API_KEY from './GIPHY_API_KEY';
 
 
 function Game({ currScore, setCurrScore, bestScore, setBestScore }) {
-    // const [cards, setCards] = useState([]);
+    const [hasPlayed, setHasPlayed] = useState(false);
+    const [prevScore, setPrevScore] = useState(0);
+
     const [cardImgUrls, setCardImgUrls] = useState([]);
+
     const [clickedCardIDs, setClickedCardIDs] = useState([]);
 
+    const loadingText = "Loading...";
+    const fetchError = "Error fetching data from server. Aborting."
+    const [statusText, setStatusText] = useState(loadingText);
+
+    async function getGiphyImages() {
+        const response = await fetch(
+            `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&rating=g&q=corgis`,
+            {
+                mode: 'cors'
+            }
+        );
+        if (!response.ok) {
+            console.log('Error fetching giphy data.', response.status)
+            setStatusText(fetchError);
+            return;
+        }
+        const imgJson = await response.json();
+        // console.log(imgJson.data);
+        const imgData = imgJson.data;
+
+        const imgUrls = [];
+        if (imgData) {
+            for (let i = 0; i < 12; i++) {
+                const entry = imgData[i];
+                // console.log(entry.images.downsized.url);
+                imgUrls.push({
+                    id: crypto.randomUUID(),
+                    url: entry.images.fixed_height_small.url
+                });
+            }
+            setCardImgUrls(imgUrls);
+        }
+    }
 
     useEffect(() => {
-        async function getGiphyImages() {
-            const response = await fetch(
-                `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&rating=g&q=corgis`,
-                {
-                    mode: 'cors'
-                }
-            );
-            if (!response.ok) {
-                console.log('Error fetching giphy data.', response.status)
-            }
-            const imgJson = await response.json();
-            // console.log(imgJson.data);
-            const imgData = imgJson.data;
-
-            const imgUrls = [];
-            if (imgData) {
-                for (let i = 0; i < 12; i++) {
-                    const entry = imgData[i];
-                    // console.log(entry.images.downsized.url);
-                    imgUrls.push({
-                        id: crypto.randomUUID(),
-                        url: entry.images.fixed_height_small.url
-                    });
-                }
-                setCardImgUrls(imgUrls);
-            }
-        }
-        
         getGiphyImages();
-
-
-        return () => {}
     }, []);
 
-    function shuffleCards() {
+    
+    useEffect(() => {
+        // Only run when currScore is reset to 0, and not on initial mount
+        if (currScore !== 0 || !hasPlayed) {
+            return;
+        }
+
+        const loseCover = document.querySelector('#lose-cover');
+        loseCover.animate([
+            {
+                display: 'grid',
+                opacity: 0
+            },
+            { opacity: 1 },
+            { opacity: 1 },
+            { opacity: 1 },
+            {
+                display: 'none',
+                opacity: 0
+            }
+        ], 1750);
+    }, [currScore])
+
+    function shuffleCardImgURLs() {
         // const shuffled = cards.slice();
         if (cardImgUrls.length === 0) {
             return [];
@@ -60,28 +88,34 @@ function Game({ currScore, setCurrScore, bestScore, setBestScore }) {
     }
 
     function createRandomCardDraw() {
-        const shuffledCards = shuffleCards();
+        const shuffledCards = shuffleCardImgURLs();
         return (
-            <div id="card-draw">
-                {/* Random Card Draw! */}
-                { !shuffledCards.length ? <div>Loading...</div> :
-                    shuffledCards.map((obj, i) => {
-                        return (
-                            <div
-                                className="card"
-                                key={obj.id}
-                                onClick={() => { onCardClick(obj.id)}}
-                            >
-                                <img src={obj.url} />
-                            </div>
-                        )
-                    })
-                }
-            </div>
+            <>
+            { !shuffledCards.length ? <div>{statusText}</div> :
+                shuffledCards.map((obj, i) => {
+                    return (
+                        <div
+                            className="card"
+                            key={obj.id}
+                            onClick={() => { onCardClick(obj.id); }}
+                        >
+                            <img src={obj.url} />
+                        </div>
+                    )
+                })
+            }
+            </>
         )
     }
 
+    function resetGame() {
+        setPrevScore(currScore);
+        setCurrScore(0);
+        setClickedCardIDs([]);
+    }
+
     function onCardClick(cardID) {
+        setHasPlayed(true);
         const alreadyClicked = clickedCardIDs.find((clickedID) => { return cardID === clickedID; });
         if (alreadyClicked) {
             // End game run.
@@ -89,16 +123,14 @@ function Game({ currScore, setCurrScore, bestScore, setBestScore }) {
             if (currScore > bestScore) {
                 setBestScore(currScore);
             }
-            // Reset game.
-            setCurrScore(0);
-            setClickedCardIDs([])
+            resetGame();
 
         } else {
             setCurrScore(currScore + 1);
             setClickedCardIDs([
                 ...clickedCardIDs,
                 cardID
-            ])
+            ]);
         }
     }
 
@@ -107,7 +139,14 @@ function Game({ currScore, setCurrScore, bestScore, setBestScore }) {
     return (
         <div id="game-output">
             <h1>Ready to play? Get clicking!</h1>
-            {randomCardDraw}
+            <div id="lose-cover">
+                Oops! Try again!<br/>
+                Your Score: {prevScore}<br/>
+                Best Score: {bestScore}
+            </div>
+            <div id="card-draw">
+                {randomCardDraw}
+            </div>
         </div>
     )
 }
